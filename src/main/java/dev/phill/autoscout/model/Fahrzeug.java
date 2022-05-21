0d17b2c8b2a088b35589c7c5ee207be2baacc11b
Phillip <jerebicp@bzz.ch>
2022-05-21 23:14:54 +0200

    added auto getter/setter

--- src/main/java/dev/phill/autoscout/model/Fahrzeug.java
+++ src/main/java/dev/phill/autoscout/model/Fahrzeug.java
@@ -2,7 +2,16 @@ package dev.phill.autoscout.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.phill.autoscout.data.DataHandler;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+//@AllArgsConstructor
 public class Fahrzeug {
 
     private String fahrzeugUUID;
@@ -20,9 +29,6 @@ public class Fahrzeug {
     private Boolean mfk;
     private Float leergewicht;
 
-    public Fahrzeug(){
-
-    }
 
     public Fahrzeug(String fahrzeugUUID, String marke, String modell, Float preis, Integer baujahr, Float hubraum,Haendler heandler, Float leistung, Boolean tuning, Boolean mfk, Float leergewicht) {
         this.fahrzeugUUID = fahrzeugUUID;
@@ -39,102 +45,14 @@ public class Fahrzeug {
     }
 
     public String getHaendlerUUID() {
-        return getHeandler().getHaendlerUUID();
+        return getHaendler().getHaendlerUUID();
     }
 
     public void setHaendlerUUID(String heandlerUUID) {
-        setHeandler( new Haendler());
+        setHaendler( new Haendler());
         Haendler haendler = DataHandler.getInstance().readHaendlerByUUID(heandlerUUID);
-        getHeandler().setHaendlerUUID(heandlerUUID);
-        getHeandler().setHaendlerUUID(haendler.getHaendlerUUID());
+        getHaendler().setHaendlerUUID(heandlerUUID);
+        getHaendler().setHaendlerUUID(haendler.getHaendlerUUID());
 
-    }
-
-    public String getMarke() {
-        return marke;
-    }
-
-    public void setMarke(String marke) {
-        this.marke = marke;
-    }
-
-    public String getModell() {
-        return modell;
-    }
-
-    public void setModell(String modell) {
-        this.modell = modell;
-    }
-
-    public Float getPreis() {
-        return preis;
-    }
-
-    public void setPreis(Float preis) {
-        this.preis = preis;
-    }
-
-    public Integer getBaujahr() {
-        return baujahr;
-    }
-
-    public void setBaujahr(Integer baujahr) {
-        this.baujahr = baujahr;
-    }
-
-    public Float getHubraum() {
-        return hubraum;
-    }
-
-    public void setHubraum(Float hubraum) {
-        this.hubraum = hubraum;
-    }
-
-    public Float getLeistung() {
-        return leistung;
-    }
-
-    public void setLeistung(Float leistung) {
-        this.leistung = leistung;
-    }
-
-    public Boolean getTuning() {
-        return tuning;
-    }
-
-    public void setTuning(Boolean tuning) {
-        this.tuning = tuning;
-    }
-
-    public Boolean getMfk() {
-        return mfk;
-    }
-
-    public void setMfk(Boolean mfk) {
-        this.mfk = mfk;
-    }
-
-    public Float getLeergewicht() {
-        return leergewicht;
-    }
-
-    public void setLeergewicht(Float leergewicht) {
-        this.leergewicht = leergewicht;
-    }
-
-    public String getFahrzeugUUID() {
-        return fahrzeugUUID;
-    }
-
-    public void setFahrzeugUUID(String fahrzeugUUID) {
-        this.fahrzeugUUID = fahrzeugUUID;
-    }
-
-    public Haendler getHeandler() {
-        return haendler;
-    }
-
-    public void setHeandler(Haendler heandler) {
-        this.haendler = heandler;
     }
 }
