26cf89642856a5b5d9be0273ddb1d8b6ad2fec85
Phillip <jerebicp@bzz.ch>
2022-06-14 13:20:24 +0200

    ...

--- src/main/java/dev/phill/autoscout/createJSON.java
+++ src/main/java/dev/phill/autoscout/createJSON.java
@@ -19,26 +19,26 @@ public class createJSON {
 
 
         Haendler[] haendlers = {
-                new Haendler(UUID.randomUUID().toString(), "franz", "meier", "bachweg"),
-                new Haendler(UUID.randomUUID().toString(), "hans", "mueller", "strasse")
+                new Haendler("eb37b55e-a2a0-4f0e-9de7-81ce506aab6f", "franz", "meier", "bachweg"),
+                new Haendler("496483fb-c64a-43ee-9b09-c7429777fef6", "hans", "mueller", "strasse")
         };
 
         Fahrzeug[] fahrzeuge = {
-                new Fahrzeug(UUID.randomUUID().toString(), "Porsche", "911 Sport Classic", 338900f, 2022, 3700f, haendlers[0], 550f, false, true, 1645f),
-                new Fahrzeug(UUID.randomUUID().toString(), "Porsche", "911 Sport Classic", 300000f, 2012, 3700f, haendlers[1], 550f, false, true, 1645f)
+                new Fahrzeug("a1d0a3b1-c0fa-4a3c-a3d5-5cf0c0fa6080", "Porsche", "911 Sport Classic", 338900f, 2022, 3700f, haendlers[0], 550f, false, true, 1645f),
+                new Fahrzeug("143c3cbc-7c1f-41b8-b52c-11f00f9627ab", "Porsche", "911 Sport Classic", 300000f, 2012, 3700f, haendlers[1], 550f, false, true, 1645f)
         };
 
         Vector<Fahrzeug> fahrzeugVector = new Vector<>();
         fahrzeugVector.add(fahrzeuge[0]);
         fahrzeugVector.add(fahrzeuge[1]);
         Merkliste[] merklistes = {
-                new Merkliste(UUID.randomUUID().toString(),fahrzeugVector, "Merkliste 1"),
-                new Merkliste(UUID.randomUUID().toString(),fahrzeugVector, "Merkliste 2")
+                new Merkliste("6f5ed8a4-33f2-424c-afca-8a76cf7a9fac",fahrzeugVector, "Merkliste 1"),
+                new Merkliste("9944014b-073f-431e-a3c3-f7979af8fa52",fahrzeugVector, "Merkliste 2")
         };
 
         Kaeufer[] kaeufers = {
-                new Kaeufer(UUID.randomUUID().toString(), 2022, merklistes[0]),
-                new Kaeufer(UUID.randomUUID().toString(), 2022, merklistes[1])
+                new Kaeufer("4cd341d9-f457-4c99-a42a-634f81efaa6e", 2022, merklistes[0]),
+                new Kaeufer("9330cc53-358c-4eba-ad47-92ef29f59d08", 2022, merklistes[1])
         };
 
         ObjectMapper om = new ObjectMapper();
